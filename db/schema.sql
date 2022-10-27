1f9a736ce918939bbd05c73465249bcf4ba7deec
DTGuy <tguy5837@gmail.com>
2022-10-27 16:47:06 -0400

    added scaffolding and started setting up routes

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS tech_blogger_db;
+CREATE DATABASE tech_blogger_db;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// GET /api/users/:id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects { username: 'abcd', email: 'abc@abc.com', password: 'abc123'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT /api/users/:id
+router.put('/:id', (req, res) => {
+    // expects { username: 'abcd', email: 'abc@abc.com', password: 'abc123'}
+
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' })
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// DELETE /api/users/:id
+router.delete('/:id', (req, res) => { });
+
+module.exports = router;
