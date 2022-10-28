7367544d5cc4bbc33d43447cc35c03025980ce32
DTGuy <tguy5837@gmail.com>
2022-10-28 15:56:24 -0400

    added user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,8 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    User.findAll()
+    console.log('Hi!');
+    User.findAll({})
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -69,6 +70,23 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/:id
-router.delete('/:id', (req, res) => { });
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
